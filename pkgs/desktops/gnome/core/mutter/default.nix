083c0ac4f11b3d69def458f7fec1d7a5da756dbe
Jan Tojnar <jtojnar@gmail.com>
2022-02-16 18:35:21 +0000

    gnome.mutter: 41.3 → 42.beta
    https://gitlab.gnome.org/GNOME/mutter/-/compare/41.3...42.beta

--- pkgs/desktops/gnome/core/mutter/default.nix
+++ pkgs/desktops/gnome/core/mutter/default.nix
@@ -46,13 +46,13 @@
 
 let self = stdenv.mkDerivation rec {
   pname = "mutter";
-  version = "41.3";
+  version = "42.beta";
 
   outputs = [ "out" "dev" "man" ];
 
   src = fetchurl {
     url = "mirror://gnome/sources/mutter/${lib.versions.major version}/${pname}-${version}.tar.xz";
-    sha256 = "cZxdEGDrYFwbpkO0hvtXsaPHGX59+DvWwzZvxLaEd+U=";
+    sha256 = "Nz3ZZtoUmh8qi2suTxsYIelEkUyaUd5A+0UTCNVD7UQ=";
   };
 
   patches = [
